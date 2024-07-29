85ce5451b77a297dc8737ae0708cb89b69ca1c8a
aolsen2001 <174476920+aolsen2001@users.noreply.github.com>
2024-07-29 14:57:31 -0500

    Add slider to change grid size

--- etchasketch.js
+++ etchasketch.js
@@ -1,10 +1,17 @@
+const DEFAULT_COLOR = 'black';
+const DEFAULT_SIZE = 16;
+
+let color = DEFAULT_COLOR;
+let size = DEFAULT_SIZE;
+
 function createGrid(size) {
     const gridContainer = document.getElementById('grid-container');
     gridContainer.innerHTML = ''; // Clear any existing grid
 
     // define the size of each grid item
     const gridSize = 480;
-    const itemSize = gridSize / size; 
+    // itemSize is rounded to one decimal place to prevent the tiles from being placed outside the boundary
+    const itemSize = Math.round((gridSize / size) * 10) / 10;
 
     // calculate the container size based on the item size and grid size
     gridContainer.style.width = `${itemSize * size}px`;
@@ -18,25 +25,32 @@ function createGrid(size) {
         div.style.width = `${itemSize}px`;
         div.style.height = `${itemSize}px`;
 
-        div.addEventListener('mouseover', (e) => {
-            e.target.style.background = "blue";
+        div.addEventListener('mouseenter', (e) => {
+            e.target.style.background = color;
         });
-        div.addEventListener('mouseleave', (e) => {
-            e.target.style.background = "white";
-        })
+        // div.addEventListener('mouseleave', (e) => {
+        //     e.target.style.background = "white";
+        // })
 
         gridContainer.appendChild(div);
     }
 }
 
-const sizeButton = document.getElementById('resize');
-sizeButton.addEventListener('Click', (e) => {
-    console.log('clicked');
-    let size = 0;
-    while (size < 0 || size > 100) {
-        size = prompt("Enter a new size between 1 and 100");
-    }
-})
-
-
 createGrid(16);
+
+const sizeButton = document.getElementById('resize');
+const sizePrompt = document.getElementById('sizeprompt');
+const slider = document.getElementById("sizeSlider");
+var sliderText = document.getElementById("sliderText");
+
+// set slider to default size of 16
+slider.value = 16;
+sliderText.innerHTML = slider.value; 
+
+// update size based on slider
+slider.oninput = function() {
+  sliderText.innerHTML = this.value;
+  createGrid(this.value);
+} 
+
+
