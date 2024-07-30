25efc69e46724bd7c61c29a1d48a63b679e823f6
aolsen2001 <174476920+aolsen2001@users.noreply.github.com>
2024-07-29 22:56:30 -0500

    Add buttons to enhance functionality, edit layout
    Modified styling

--- etchasketch.js
+++ etchasketch.js
@@ -1,12 +1,14 @@
 const DEFAULT_COLOR = 'black';
 const DEFAULT_SIZE = 16;
 
-let color = DEFAULT_COLOR;
-let size = DEFAULT_SIZE;
-
+/**
+ * Create a new grid with the given size
+ * 
+ * @param {number} size the size of the new grid
+ */
 function createGrid(size) {
-    const gridContainer = document.getElementById('grid-container');
-    gridContainer.innerHTML = ''; // Clear any existing grid
+    // remove exisiting grid
+    gridContainer.innerHTML = ''; 
 
     // define the size of each grid item
     const gridSize = 480;
@@ -17,7 +19,7 @@ function createGrid(size) {
     gridContainer.style.width = `${itemSize * size}px`;
     gridContainer.style.height = `${itemSize * size}px`;
 
-    // create the grid items
+    // create the grid items and append them to the gridContainer
     for (let i = 0; i < size * size; i++) {
         const div = document.createElement('div');
         div.classList.add('grid-item');
@@ -25,32 +27,84 @@ function createGrid(size) {
         div.style.width = `${itemSize}px`;
         div.style.height = `${itemSize}px`;
 
+        // allow for color to change on mouseenter
         div.addEventListener('mouseenter', (e) => {
             e.target.style.background = color;
         });
-        // div.addEventListener('mouseleave', (e) => {
-        //     e.target.style.background = "white";
-        // })
 
         gridContainer.appendChild(div);
     }
 }
 
-createGrid(16);
+function clearGrid () {
+    const tiles = gridContainer.children;
+    for (var i = 0; i < tiles.length; ++i) {
+        tiles[i].style.background = "white";
+    }
+}
 
-const sizeButton = document.getElementById('resize');
-const sizePrompt = document.getElementById('sizeprompt');
+/**
+ * Toggle the eraser on
+ */
+function setEraser () {
+    const tiles = gridContainer.children;
+    for (var i = 0; i < tiles.length; ++i) {
+        tiles[i].addEventListener('mouseenter', (e) => {
+            e.target.style.background = "white";
+        });
+    }
+}
+
+/**
+ * Toggle the eraser off
+ */
+function setBrush () {
+    const tiles = gridContainer.children;
+    for (var i = 0; i < tiles.length; ++i) {
+        tiles[i].addEventListener('mouseenter', (e) => {
+            e.target.style.background = color;
+        });
+    }
+}
+
+let color = DEFAULT_COLOR;
+let size = DEFAULT_SIZE;
+
+const gridContainer = document.getElementById('grid-container');
+const eraserToggle = document.getElementById('eraserToggle');
+const clearButton = document.getElementById('clearButton');
 const slider = document.getElementById("sizeSlider");
 var sliderText = document.getElementById("sliderText");
 
 // set slider to default size of 16
-slider.value = 16;
-sliderText.innerHTML = slider.value; 
+slider.value = size;
+sliderText.innerHTML = ` ${size} x ${size}`;
+
+createGrid(size);
+
+clearButton.addEventListener("click", () => {
+    clearGrid();
+})
+
+var eraserOn = false;
+eraserToggle.addEventListener("click", () => {
+    if (eraserOn === false) {
+        setEraser();
+        eraserOn = true;
+        eraserToggle.textContent = 'Eraser On';
+    } else {
+        setBrush();
+        eraserOn = false;
+        eraserToggle.textContent = 'Eraser Off';
+    }
+})
+
 
 // update size based on slider
-slider.oninput = function() {
-  sliderText.innerHTML = this.value;
-  createGrid(this.value);
-} 
+slider.oninput = function () {
+    size = slider.value;
+    sliderText.innerHTML = ` ${size} x ${size}`;
+    createGrid(size);
+}
 
 
